e7428f83c3679cab80909fde03306f2ae8ed9e5f
Mihirpndya <mpandya284@gmail.com>
2024-09-14 17:06:26 +0530

    Made changes with the sideBar opening and closing without redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,35 @@ import Navbar from "../components/Navbar";
 import { useState } from "react";
 
 const Home = () => {
-  const [openBar, SetOpenBar] = useState(false);
-  console.log(openBar)// these are my changes
+	const [openBar, SetOpenBar] = useState(false);
+	console.log(openBar); // these are my changes
 
-  const toggleOpen = () => {
-    SetOpenBar(!openBar);
-  };
-  
-  return (
-    <div>
-      <div className={`sticky top-0 ${openBar ? "bg-black text-zinc-800 opacity-95" : ""}`}>
-        <Navbar toggleOpen={toggleOpen} />
-      </div>
-      <div className="bg-zinc-900 text-white z-[20]">
-        <Sidebar openBar={openBar} toggleOpen={toggleOpen} />
-      </div>
-    </div>
-  );
+	const toggleOpen = () => {
+		SetOpenBar(!openBar);
+	};
+
+	return (
+		<div>
+			<div
+				className={`sticky top-0 ${
+					openBar ? "bg-black text-zinc-800 opacity-95" : ""
+				}`}
+			>
+				<Navbar toggleOpen={toggleOpen} SetOpenBar={SetOpenBar} />
+			</div>
+			<div className="flex bg-zinc-900">
+				<div className=" text-white z-[20]">
+					<Sidebar openBar={openBar} toggleOpen={toggleOpen} />
+				</div>
+				<div
+					className="w-full h-ful"
+					onClick={() => {
+						SetOpenBar(false);
+					}}
+				> Hi there</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
